071141d18228dd97c6fd3450d8f730be5672a5d2
guirbm <guirbm@gmail.com>
2026-07-31 14:23:23 -0300

    atualizações ocs e entregas 1423

--- index.js
+++ index.js
@@ -530,7 +530,7 @@ app.post('/romaneios', authMiddleware, async (req, res) => {
 app.get('/romaneios', authMiddleware, async (req, res) => {
   try {
     const sql = `
-      SELECT ocromaneio , data_criacao , TO_CHAR(data_entregasaida, 'DD/MM/YYYY') as data_entregasaida , motorista , duracaoest , kmest , status , qtdentregas , qtdfinalizadas , obs 
+      SELECT ocromaneio , data_criacao , data_entregasaida , motorista , duracaoest , kmest , status , qtdentregas , qtdfinalizadas , obs 
 	  FROM public.romaneios
       order by ocromaneio desc	   
     `;
